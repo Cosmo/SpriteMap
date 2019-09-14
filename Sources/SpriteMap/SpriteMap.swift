3822adf9d1dd5893679c3ec85e61d0f8d581b341
Devran "Cosmo" Uenal <maccosmo@gmail.com>
2019-09-14 10:40:10 +0200

    Add CGImage merger

--- Sources/SpriteMap/SpriteMap.swift
+++ Sources/SpriteMap/SpriteMap.swift
@@ -1,3 +1,34 @@
-struct SpriteMap {
-    var text = "Hello, World!"
+import Foundation
+import CoreGraphics
+
+extension CGImage {
+    static func mergeImages(_ images: [CGImage]) -> CGImage? {
+        guard let firstImage = images.first else { return nil }
+        let width = firstImage.width
+        let height = firstImage.height
+        
+        var data = Data(capacity: 0)
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        let closure = { (bytes: UnsafeMutableRawBufferPointer) -> CGImage? in
+            guard let context = CGContext(data: nil,
+                                          width: width,
+                                          height: height,
+                                          bitsPerComponent: 8,
+                                          bytesPerRow: 0,
+                                          space: colorSpace,
+                                          bitmapInfo: 1) else {
+                                            return nil
+            }
+            for image in images {
+                let size = CGSize(width: image.width, height: image.height)
+                let rect = CGRect(origin: CGPoint.zero, size: size)
+                context.draw(image, in: rect)
+            }
+            return context.makeImage()
+        }
+        
+        return data.withUnsafeMutableBytes(closure)
+    }
 }
+
